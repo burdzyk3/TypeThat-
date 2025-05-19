4245f9dfdaacd24a12537b794022f8b3fa7a0e77
Wiktor <dsw51733@student.dsw.edu.pl>
2025-05-19 23:13:08 +0200

    Added hamburger button for phones and working animations to open/close menu, added hamburger svg to img folder

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ function decrementScore(id) {
 		input.value = parseInt(input.value) - 1;
 	}
 }
+
+function toggleMenu() {
+	nav.classList.toggle('active');
+	body.classList.toggle('no-scroll');
+	hamburger.classList.toggle('hamburgerAnimation');
+}
+
+
+const body = document.body;
+const nav = document.querySelector('.navLinks');
+const hamburger = document.querySelector('.hamburger');
+
+
+const navLi = document.querySelectorAll('.navLi');
+console.log(navLi);
+navLi.forEach(item => {
+	item.addEventListener('click', () =>{
+		nav.classList.remove('active');
+		body.classList.remove('no-scroll');
+		hamburger.classList.remove('hamburgerAnimation');
+	})
+})
